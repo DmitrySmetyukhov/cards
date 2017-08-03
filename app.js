602acc0a1d11073f49a72392642e04498b68d68e
DmitrySmetyukhov <dmitry.smetyukhov@gmail.com>
2017-08-03 13:07:30 +0300

    https

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ var app = express();
 
 const forceSSL = function() {
     return function (req, res, next) {
-        if (req.headers['x-forwarded-proto'] !== 'https') {
+        if (!req.headers['x-forwarded-proto'] || req.headers['x-forwarded-proto'] !== 'https') {
             return res.redirect(
                 ['https://', req.get('Host'), req.url].join('')
             );
