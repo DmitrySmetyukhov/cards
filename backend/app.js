eba1880e2f423372561001acb553e528f17a2a32
DmitrySmetyukhov <dmitry.smetyukhov@gmail.com>
2017-08-04 17:35:16 +0300

    settings

--- backend/app.js
+++ backend/app.js
@@ -16,6 +16,22 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 
+const forceSSL = function() {
+    return function (req, res, next) {
+        if (req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
+
 app.use(function (req, res, next) {
     res.header('Access-Control-Allow-Origin', req.headers.origin);
     res.header('Access-Control-Allow-Credentials', true);
