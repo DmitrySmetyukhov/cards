6e3d2131f42feb009c6c950250be814ff441f084
DmitrySmetyukhov <dmitry.smetyukhov@gmail.com>
2017-08-09 21:08:02 +0300

    settings

--- app.js
+++ app.js
@@ -16,18 +16,18 @@ var categories = require('./routes/categories');
 var app = express();
 
 
-// const forceSSL = function() {
-//     return function (req, res, next) {
-//         if (!req.headers['x-forwarded-proto'] || req.headers['x-forwarded-proto'] !== 'https') {
-//             return res.redirect(
-//                 ['https://', req.get('Host'), req.url].join('')
-//             );
-//         }
-//         next();
-//     }
-// };
-//
-// app.use(forceSSL());
+const forceSSL = function() {
+    return function (req, res, next) {
+        if (!req.headers['x-forwarded-proto'] || req.headers['x-forwarded-proto'] !== 'https') {
+            return res.redirect(
+                ['https://', req.get('Host'), req.url].join('')
+            );
+        }
+        next();
+    }
+};
+
+app.use(forceSSL());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
